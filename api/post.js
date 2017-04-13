49b4d0d3218a6773ad9ed5e0a3d7e58fcbff2203
albertusk95 <albertusk15@gmail.com>
2017-04-13 19:00:41 +0700

    add API for creating new post

--- api/post.js
+++ api/post.js
@@ -3,7 +3,7 @@ const Post = require("../models/posts");
 module.exports = function(router){
 	
 	// API list all posts
-	router.get("/posts", function(req, res){
+	router.get("/posts", function(req, res) {
 		Post.list(function(err, posts){			
 			if (err) {	
 				res.status(500);
@@ -15,7 +15,7 @@ module.exports = function(router){
 	});
 	
 	// API get post by id
-	router.get("/posts/:_id", function(req, res){
+	router.get("/posts/:_id", function(req, res) {
 		const _id = req.params._id;
 		Post.get(_id, function(err, post){			
 			if (err) {	
@@ -25,6 +25,31 @@ module.exports = function(router){
 				res.json({ "_data": post });		
 			}
 		});
+	});
+	
+	// API create new post
+	// 
+	// URL endpoint: 
+	// POST /api/posts
+	// 
+	// Body (URL-encoded)
+	// title=<title>&content=<content>&author=<author>
+	
+	router.post("/posts", function(req, res) {
+		
+		var title = req.body.title;
+		var content = req.body.content;
+		var author = req.body.author;
+		
+		Post.create(title, content, author, function(err, posts){			
+			if (err) {	
+				res.status(500);
+				res.json({_message: err});
+			} else {
+				res.json({ "_data": posts });		
+			}
+		});
+		
 	});
 
 };

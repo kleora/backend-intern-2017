1d5febc974f900480372b3fd7d62ed0a6271f5f2
rapid <fidrafid@gmail.com>
2017-04-17 22:52:41 +0700

    add comment model

---
+++ models/comments.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+/* Comment schema */
+const schema = new Schema({
+	_id: Schema.ObjectId,
+	author: String,
+	comment: String,
+    post_id: String,
+	timestamp: Date
+});
+
+var Comment = mongoose.model('comments', schema);
+
+/* Create new comment */
+Comment.create = function (author, comment, post_id, callback){
+	let komen = new Comment({
+		_id: new ObjectId(),
+		author, comment, post_id,
+		timestamp: new Date()
+	});
+	
+	komen.save(function(err, obj){
+		if(err || !obj){
+			return callback(err);
+		}
+		callback(null, obj.toObject());
+	});
+};
+
+/* List all comments sorted by creation time */
+Comment.list = function (_id, callback){
+	Comment.find({post_id : _id}, null, {sort: {timestamp: -1}}, function(err, comments){
+		if (err) return callback(err);
+		
+		callback(null, comments.map(obj => obj.toObject()));
+	});
+};
+
+module.exports = Comment;
